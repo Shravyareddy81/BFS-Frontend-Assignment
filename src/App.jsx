afddb295d6bbbfb33dc46195ee82303d5a26824d
Shravyareddy81 <shravyareddy1121@gmail.com>
2026-03-26 10:54:55 +0530

    test render

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,5 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Dashboard from "./pages/Dashboard";
-import UserDetail from "./pages/UserDetail";
-
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/user/:id" element={<UserDetail />} />
-    </Routes>
-  );
+  return <h1>Hello Shravya ✅</h1>;
 }
 
 export default App;
