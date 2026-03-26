88ee4ab7653362197f4a64e02466f4ca7b5a6492
Shravyareddy81 <shravyareddy1121@gmail.com>
2026-03-26 07:44:46 +0530

    fixed build error

--- user-dashboard/src/pages/Home.jsx
+++ user-dashboard/src/pages/Home.jsx
@@ -4,18 +4,18 @@ const Home = () => {
   const navigate = useNavigate();
 
   return (
-    <div style={{ textAlign: "center", marginTop: "100px" }}>
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
       <h1>Welcome!!</h1>
 
-      <button
-        onClick={() => navigate("/dashboard")}
-        style={{
-          padding: "10px 20px",
-          marginTop: "20px",
-          fontSize: "16px",
-          cursor: "pointer",
-        }}
-      >
+      <button onClick={() => navigate("/dashboard")}>
         Go to Dashboard
       </button>
     </div>
